2c1f2112559287016439a0821133121b98e83ed6
Chua Chin Siang <ccs6327@gmail.com>
2016-01-30 13:26:02 -0800

    updated event handler

---
+++ event.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class Event(Enum):
+	AngryTreeImps = 0
+	ArmsBan = 1
+	MinersStrike = 2
+	HappyTreeImps = 3
+	ArmsRace = 4
+	ShovelItUp = 5
+	BlackSwan = 6
+	GoodHarvest = 7
+	SharingIsCaring = 8
+	BankRupt = 9
+	DwarvesDay = 10
