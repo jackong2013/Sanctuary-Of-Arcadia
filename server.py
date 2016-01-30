10f6cb2651c44c052479a766f39e71085aa0d9f7
Jack Ong <jackong2013@gmail.com>
2016-01-30 10:07:12 -0800

    Added server file

---
+++ server.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on available packages.
+async_mode = None
+
+if async_mode is None:
+    try:
+        import eventlet
+        async_mode = 'eventlet'
+    except ImportError:
+        pass
+
+    if async_mode is None:
+        try:
+            from gevent import monkey
+            async_mode = 'gevent'
+        except ImportError:
+            pass
+
+    if async_mode is None:
+        async_mode = 'threading'
+
+    print('async_mode is ' + async_mode)
+
+# monkey patching is necessary because this application uses a background
+# thread
+if async_mode == 'eventlet':
+    import eventlet
+    eventlet.monkey_patch()
+elif async_mode == 'gevent':
+    from gevent import monkey
+    monkey.patch_all()
+
+import time
+from threading import Thread
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+
+@app.route('/<username>', methods=['GET', 'POST'])
+def login(username):
+	if request.method == 'POST':
+		pass
+	return "this is the login screen: " + username
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, host='0.0.0.0')
