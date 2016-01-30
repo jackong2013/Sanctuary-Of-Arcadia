e1ee667e1cb255c5ef07fbdb1f6e12ff864c03b7
Jack Ong <jackong2013@gmail.com>
2016-01-30 14:06:26 -0800

    no message

--- player.py
+++ player.py
@@ -1,39 +1,4 @@
-INVENTORY_CAP = 10
-WOOD = 'wood'
-IRON = 'iron'
-GOLD = 'gold'
-LUMBER = 'lumber'
-MITHRIL = 'mithril'
-TREASURE = 'treasure'
-WOODMILL = 'woodmill'
-IRON_FORGE = 'ironForge'
-GOLD_MINE = 'goldMine'
-LUMBER_MILL = 'lumberMill'
-MITHRIL_FORGE = 'mithrilForge'
-TREASURE_MINE = 'treasureMine'
-GRAND_SANCTUARY = 'grandSanctuary'
-PHOENIX_FORGE = 'phoenixForge'
-BANK_SMAUG = 'bankSmaug'
-PEGASUS_CRUISER = 'pegasusCruiser'
-FIRST_TIER_RESOURCE = [WOOD, IRON, GOLD]
-SECOND_TIER_RESOURCE = [LUMBER, MITHRIL, TREASURE]
-FIRST_TIER_GENERATOR = { WOODMILL: 1, IRON_FORGE: 1, GOLD_MINE: 1 }
-SECOND_TIER_GENERATOR = { LUMBER_MILL: 3, MITHRIL_FORGE: 3, TREASURE_MINE: 3}
-COST = {
-	LUMBER: { WOOD: 2 },
-	MITHRIL: { IRON: 2 },
-	TREASURE: {	GOLD: 2 },
-	WOODMILL: { WOOD: 3, IRON: 1, GOLD: 1 },
-	IRON_FORGE: { WOOD: 1, IRON: 3, GOLD: 1 },
-	GOLD_MINE: { WOOD: 1, IRON: 1, GOLD: 3 },
-	LUMBER_MILL: { WOODMILL: 1, LUMBER: 1, IRON: 1, GOLD: 1},
-	MITHRIL_FORGE: { IRON_FORGE: 1, MITHRIL: 1, WOOD: 1, GOLD: 1},
-	TREASURE_MINE: { GOLD_MINE: 1, TREASURE: 1, WOOD: 1, IRON: 1},
-	GRAND_SANCTUARY: { LUMBER: 5, TREASURE: 2, MITHRIL: 2},
-	PHOENIX_FORGE: { LUMBER: 2, TREASURE: 2, MITHRIL: 5},
-	BANK_SMAUG: { LUMBER: 2, TREASURE: 5, MITHRIL: 2},
-	PEGASUS_CRUISER: { LUMBER: 3, TREASURE: 3, MITHRIL: 3}
-}
+import logicHandler as lgc
 
 class Player(object):
 
@@ -42,20 +7,20 @@ class Player(object):
 		self.objective = objective
 		self.inventoryFlag = False
 		self.resources = {
-			WOOD: 3,
-			IRON: 3,
-			GOLD: 3,
-			LUMBER: 0,
-			MITHRIL: 0,
-			TREASURE: 0
+			lgc.WOOD: 3,
+			lgc.IRON: 3,
+			lgc.GOLD: 3,
+			lgc.LUMBER: 0,
+			lgc.MITHRIL: 0,
+			lgc.TREASURE: 0
 		}
 		self.generators = {
-			WOODMILL: 0,
-			IRON_FORGE: 0,
-			GOLD_MINE: 0,
-			LUMBER_MILL: 0,
-			MITHRIL_FORGE: 0,
-			TREASURE_MINE: 0
+			lgc.WOODMILL: 0,
+			lgc.IRON_FORGE: 0,
+			lgc.GOLD_MINE: 0,
+			lgc.LUMBER_MILL: 0,
+			lgc.MITHRIL_FORGE: 0,
+			lgc.TREASURE_MINE: 0
 		}
 
 	# Accessor for the name
@@ -84,29 +49,62 @@ class Player(object):
 		else:
 			return 'WRONG GENERATOR NAME!!! This game does not have ' + generator
 
-	# Accessor to check if there is space in the inventory
+	# Mutator to update the number of a particular resource
+	def update_resource(self, item, quantity):
+		self.resources[item] += quantity
+		return self.resources[item]
+		# if item in self.resources.keys():
+		# 	temp = self.resources[item] + quantity
+		# 	if temp < 0:
+		# 		return "YOU DO NOT HAVE ENOUGH " + item
+		# 	else:
+		# 		self.resources[item] = temp
+		# 		self.inventoryFlag = False if self.inventory_vacant() else True
+		# elif item in self.generators.keys():
+		# 	temp = self.generators[item] + quantity
+		# 	if temp < 0:
+		# 		return "YOU DO NOT HAVE ENOUGH " + item
+		# 	else:
+		# 		self.generators[item] = temp
+		# else:
+		# 	return 'WRONG ITEM NAME!!! This game does not have ' + item
+
+	# Mutator to update the number of a particular resource
+	def update_generator(self, item, quantity):
+		self.generators[item] += quantity
+		return self.generators[item]
+
+	# Method to check if there is space in the inventory
 	def inventory_vacant(self):
 		count = 0
 		for res in self.resources.keys():
-			if res in FIRST_TIER_RESOURCE:
+			if res in lgc.FIRST_TIER_RESOURCE:
 				count += self.resources[res]
-			if count > INVENTORY_CAP:
+			if count > lgc.INVENTORY_CAP:
 				return False
 		return True
 
-	# Mutator to update the number of a particular resource
-	def update_resource(self, resource, quantity):
-		if resource in self.resources.keys():
-			temp = self.resources[resource] + quantity
-			if temp < 0:
-				return "YOU DO NOT HAVE ENOUGH " + resource
+	# Method to check if the player has enough resources to build an item
+	def ingredient_suffice(self, item):
+		can_build = True
+		for ingredient, qty in lgc.COST[item].items():
+			if qty > self.get_resources()[ingredient]:
+				can_build = False
+				return can_build
+		return can_build
+		
+
+	def build(self, item):
+		if item in COST.keys():
+			if self.ingredient_suffice(item) and item = self.objective:
+				return "GAME OVER"
+			elif self.ingredient_suffice(item):
+				for ingredient, qty in COST[item].items():
+					self.update_item(ingredient, -1*qty)
+				self.update_item(item, 1)
 			else:
-				self.resources[resource] = temp
-				self.inventoryFlag = False if self.inventory_vacant() else True
+				return "NOT ENOUGH INGREDIENT TO BUILD " + item
 		else:
-			return 'WRONG RESOURCE NAME!!! This game does not have ' + resource
+			return 'WRONG ITEM NAME!!! This game does not have ' + item
 
-	def build(self):
-		pass
-
-
+	def gather(self):
